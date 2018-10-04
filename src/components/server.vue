ced2f822675582230fddc433ddb9fed0a75d5506
Unknown <riderjensen@gmail.com>
2018-10-04 10:26:42 -0600

    Updating eventBus

--- src/components/server.vue
+++ src/components/server.vue
@@ -1,47 +1,53 @@
 <template>
-  <v-layout row>
-	  <v-flex xs3></v-flex>
-    <v-flex xs9>
-        <v-list two-line>
-          <template v-for="item in list">
-            <v-list-tile
-              :key="item"
-              @click="eventBus.$emit('send-data', item.Status)"
-            >
-              <v-list-tile-content>
-                <v-list-tile-title v-html="item.Name"></v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </template>
-        </v-list>
-    </v-flex>
-  </v-layout>
+	<v-layout row>
+		<v-flex xs3></v-flex>
+		<v-flex xs9>
+				<v-list two-line>
+					<template v-for="item in list">
+						<v-list-tile
+							:key="item.Name"
+							@click="emitMeCaptain(item.Status)"
+						>
+							<v-list-tile-content>
+								<v-list-tile-title v-html="item.Name"></v-list-tile-title>
+							</v-list-tile-content>
+						</v-list-tile>
+					</template>
+				</v-list>
+		</v-flex>
+	</v-layout>
 </template>
 
 <script>
 import { eventBus } from "../main";
 export default {
-  name: "server",
-  props: {
-    msg: String
-  },
-  data: () => {
-    return {
-      list: [
-        { Name: "Server One", Status: "Good" },
-        { Name: "Server Two", Status: "Bad" },
-        { Name: "Server Three", Status: "On Fire" },
-        { Name: "Server Four", Status: "Flooded" },
-        { Name: "Server Five", Status: "Beehive" }
-      ]
-    };
-  }
+	name: "server",
+	props: {
+		msg: String
+	},
+	data: () => {
+		return {
+			list: [
+				{ Name: "Server One", Status: "Good" },
+				{ Name: "Server Two", Status: "Bad" },
+				{ Name: "Server Three", Status: "On Fire" },
+				{ Name: "Server Four", Status: "Flooded" },
+				{ Name: "Server Five", Status: "Beehive" }
+			]
+		};
+	},
+	methods: {
+		emitMeCaptain(status){
+			console.log('Emit');
+			eventBus.$emit('send-data', status)
+		}
+	}
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 * {
-  text-align: center;
+	text-align: center;
 }
 </style>
